390213926dfabada5038cf7403b706ed0dc8d751
uesleijf <uesleijf@gmail.com>
2020-04-17 13:15:24 -0300

    - Configure config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+//Informações para conexão com o banco de dados
+$servidor = "localhost";
+$tipo_servidor = "mysql";
+$nome_do_banco = "chat";
+$usuario = "root";
+$senha = "root";
+ 
+//instancia um objeto da classe PDO chamado $conn
+$conn = new PDO("$tipo_servidor:host=$servidor;dbname=$nome_do_banco",$usuario,$senha); 
