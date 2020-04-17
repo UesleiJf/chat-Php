b2d0cc402cb98cf3e4efc863079172cec64bcda9
uesleijf <uesleijf@gmail.com>
2020-04-17 14:54:58 -0300

    - Variáveis e funções pro login

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 session_start();
 require_once("config.php"); 
+require_once("functions.php");
+
+//Pega o nome e a sala que o usuário soliciou entrar
+$nome = isset($_POST["txtNome"])?strip_tags($_POST["txtNome"]):"";
+$sala = isset($_POST["slSala"])?(int)$_POST["slSala"]:1;
  
+//Se o nome não estiver em branco, executa uma rotina de limpeza delete_olde_entries() e inicia o chat.
+if(!empty($nome)){
+    delete_old_entries();
+    start_chat();
+}
+
 ?>
 <html>
 <head>
@@ -33,6 +44,14 @@ require_once("config.php");
     <input type="text" name="txtNome" id="txtNome" class="form-control margininferior" placeholder="Nome" required autofocus>
     <label class="alinharesquerda">Sala</label>
     <select class="btn btn-info dropdown-toggle alinharesquerda" type="button" name="slSala" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" placeholder="Sala" required>
+    <?php
+    //Lista todas as salas cadastradas no banco de dados
+    $tbSala = $conn->prepare("select * from salas");
+    $tbSala->execute();
+    while($linha=$tbSala->fetch(PDO::FETCH_ASSOC)){
+        echo "<option value='$linha[id_sala]'>$linha[nm_sala]</option>";
+    }
+    ?>
     </select>
     <label class="alinhardireita">
     <input type="submit" name="btnEntrar" id="btnEntrar" value="Entrar" class="btn btn-success"/>
