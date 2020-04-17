f3ebca5657f98d70cf31fbf24707e3aff0c311ce
uesleijf <uesleijf@gmail.com>
2020-04-17 14:54:39 -0300

    - Refactory

--- config.php
+++ config.php
@@ -6,5 +6,6 @@ $nome_do_banco = "chat";
 $usuario = "root";
 $senha = "root";
  
+
 //instancia um objeto da classe PDO chamado $conn
-$conn = new PDO("$tipo_servidor:host=$servidor;dbname=$nome_do_banco",$usuario,$senha); 
+$conn = new PDO("$tipo_servidor:host=$servidor;dbname=$nome_do_banco",$usuario,$senha);
