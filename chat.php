c565d21037054fc46a83394e7ae4f6d24b4cb4ff
uesleijf <uesleijf@gmail.com>
2020-04-19 20:33:15 -0300

    - Layout sala bate papo

--- chat.php
+++ chat.php
@@ -36,6 +36,7 @@ if(isset($_POST["btnEnviar"]) && isset($_POST["txtMensagem"])){
 <!-- Bootstrap core CSS -->
     <link href="bootstrap-4.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/estilo.css" rel="stylesheet">
+    <meta charset="utf-8"/>
 
     <!-- Custom styles for this template -->
     <link href="signin.css" rel="stylesheet">
@@ -50,17 +51,14 @@ if(isset($_POST["btnEnviar"]) && isset($_POST["txtMensagem"])){
 </style>
 </head>
 <body>
+<a class="btn btn-danger alinhardireita margemdireita" href="sair.php">Sair da Sala</a>
+
 <div class="cabecalho">
-    <h1>Chat Online</h1>
+    <p><b>Chat Online</b></p> Sala: <b><?php echo pega_nome_sala($_SESSION["sala"]);?> </b>
 </div>
 
 <div style="text-align:center">
- 
 
- 
-<h2 style="color:#0C3">Você está na Sala <?php echo pega_nome_sala($_SESSION["sala"]);?> 
-<a class="btn btn-danger" href="sair.php">Sair da Sala</a></h2>
- 
 <hr />
 <form action="chat.php" method="post">
 <table width="709" border="1" align="center" cellpadding="0" cellspacing="0">
