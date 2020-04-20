1fe9aed91a4b9d18bed478cdc2cb30ac3a5075c2
uesleijf <uesleijf@gmail.com>
2020-04-20 20:59:29 -0300

    -identação index

--- chat.php
+++ chat.php
@@ -31,47 +31,47 @@ if(isset($_POST["btnEnviar"]) && isset($_POST["txtMensagem"])){
  
 ?>
 <html>
-<head>
-<title>Chat</title>
-<!-- Bootstrap core CSS -->
-    <link href="bootstrap-4.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/estilo.css" rel="stylesheet">
-    <meta charset="utf-8"/>
+    <head>
+        <title>Chat</title>
+        <!-- Bootstrap core CSS -->
+            <link href="bootstrap-4.0/css/bootstrap.min.css" rel="stylesheet">
+            <link href="css/estilo.css" rel="stylesheet">
+            <meta charset="utf-8"/>
 
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
-<style>
-.tab{
-    background-color:#000;
-    color:#FFF;
-    font-size:12px;
-    font-weight:bold;
-    padding:4px;
-}
-</style>
-</head>
-<body>
-<a class="btn btn-danger alinhardireita margemdireita" href="sair.php">Sair da Sala</a>
+            <!-- Custom styles for this template -->
+            <link href="signin.css" rel="stylesheet">
+        <style>
+        .tab{
+            background-color:#000;
+            color:#FFF;
+            font-size:12px;
+            font-weight:bold;
+            padding:4px;
+        }
+        </style>
+    </head>
+    <body>
+        <a class="btn btn-danger alinhardireita margemdireita" href="sair.php">Sair da Sala</a>
 
-<div class="cabecalho">
-    <p><b>Chat Online</b></p> Sala: <b><?php echo pega_nome_sala($_SESSION["sala"]);?> </b>
-</div>
+        <div class="cabecalho">
+            <p><b>Chat Online</b></p> Sala: <b><?php echo pega_nome_sala($_SESSION["sala"]);?> </b>
+        </div>
 
-<div style="text-align:center">
+        <div style="text-align:center">
 
-<hr />
-<form action="chat.php" method="post">
-<table width="709" border="1" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td width="516"><iframe src="interacao.php" width="500px" height="500px" frameborder="0" scrolling="yes"></iframe> </td>
-    <td width="4"> </td>
-    <td width="189"><?php require_once("users-online.php");?> </td>
-  </tr>
-  <tr>
-    <td colspan="3"><?php require_once("writing.php");?> </td>
-    </tr>
-</table>
-</form>
-</div>
-</body>
+        <hr />
+        <form action="chat.php" method="post">
+        <table width="709" border="1" align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td width="516"><iframe src="interacao.php" width="500px" height="500px" frameborder="0" scrolling="yes"></iframe> </td>
+            <td width="4"> </td>
+            <td width="189"><?php require_once("users-online.php");?> </td>
+          </tr>
+          <tr>
+            <td colspan="3"><?php require_once("writing.php");?> </td>
+            </tr>
+        </table>
+        </form>
+        </div>
+    </body>
 </html>
