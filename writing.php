b3aeb54b73124319e71f57410312997d8443b728
uesleijf <uesleijf@gmail.com>
2020-04-22 10:25:16 -0300

    - Refatorando layout

--- writing.php
+++ writing.php
@@ -6,13 +6,15 @@
    }
     
 ?>
-<table width="558" border="0" cellpadding="0" cellspacing="0">
-   <tr>
-      <td width="377"><label>
-         <textarea name="txtMensagem" cols="60" rows="3" id="txtMensagem"></textarea>
-         </label>
+<table width="558" border="0" cellpadding="0" cellspacing="0" class="tabelasemborda">
+   <tr class="tabelasemborda">
+      <td width="350" class="tabelasemborda">
+        <label>
+          <textarea name="txtMensagem" cols="55" rows="3" id="txtMensagem" style="resize: none"></textarea>
+        </label>
       </td>
-      <td width="27"> </td>
-      <td width="154"><input type="submit" name="btnEnviar" id="btnEnviar" value="Enviar Mensagem" style="padding-left:4px" /></td>
+      <td  width="154" class="centro tabelasemborda">
+        <input type="submit" name="btnEnviar" id="btnEnviar" value="Enviar" class="btn btn-success"/>
+      </td>
    </tr>
 </table>
