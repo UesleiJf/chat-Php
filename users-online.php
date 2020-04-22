e8378b8671a526dcfbdc210113aadad0f8935526
uesleijf <uesleijf@gmail.com>
2020-04-22 10:25:01 -0300

    - refatorando layout

--- users-online.php
+++ users-online.php
@@ -11,7 +11,7 @@ $tbUsers = $conn->prepare("select * from usuarios where id_sala=:sala");
 $tbUsers->bindParam(":sala", $_SESSION["sala"], PDO::PARAM_STR);
 $tbUsers->execute();
  
-echo "<select name='slUsers' id='slUsers' size='30' style='width:180px' >";
+echo "<select name='slUsers' id='slUsers' size='30' style='width:180px; height:400px;' >";
 while($l_users = $tbUsers->fetch(PDO::FETCH_ASSOC)){
     echo "<option value='$l_users[nm_usuario]' " . isSelected($to, $l_users["nm_usuario"]) . ">$l_users[nm_usuario]</option>";
 }
