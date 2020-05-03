7f8b80193a8f1bee4ef7821ba6e7cc262b8e8900
uesleijf <uesleijf@gmail.com>
2020-05-03 18:16:11 -0300

    - Refactory congif

--- config.php
+++ config.php
@@ -11,5 +11,5 @@
 				array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
 		              PDO::ATTR_PERSISTENT => false,
 		              PDO::ATTR_EMULATE_PREPARES => false,
-		              PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+		              PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', // UTF-8 para acentuação
             		));
