5386252d0f7e6a12448a99701ee2b33b08e8f223
uesleijf <uesleijf@gmail.com>
2020-05-10 20:32:45 -0300

    - config.php

--- config.php
+++ config.php
@@ -5,7 +5,6 @@
 	$nome_do_banco = "chat";
 	$usuario = "root";
 	$senha = "root";
-
 	//instancia um objeto da classe PDO chamado $conn
 	$conn = new PDO("$tipo_servidor:host=$servidor;dbname=$nome_do_banco",$usuario,$senha, 
 				array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
