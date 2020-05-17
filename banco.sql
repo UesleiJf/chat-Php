3637114e291329369079afa17b0d21a23bfe8e10
uesleijf <uesleijf@gmail.com>
2020-05-17 17:33:34 -0300

    - bd refactory

--- banco.sql
+++ banco.sql
@@ -1,7 +1,3 @@
---
--- Estrutura da tabela `interacoes`
---
-
 CREATE TABLE IF NOT EXISTS `interacoes` (
   `nm_usuario` varchar(20) NOT NULL,
   `id_sala` int(11) NOT NULL,
@@ -9,10 +5,6 @@ CREATE TABLE IF NOT EXISTS `interacoes` (
   `ds_interacao` varchar(500) NOT NULL,
   `nm_destinatario` varchar(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Estrutura da tabela `salas`
---
 
 CREATE TABLE IF NOT EXISTS `salas` (
   `id_sala` int(11) NOT NULL AUTO_INCREMENT,
@@ -24,11 +16,6 @@ INSERT INTO `salas` (`id_sala`, `nm_sala`) VALUES
 (1, 'Programação'),
 (2, 'Jogos'),
 (3, 'Música');
-
--- --------------------------------------------------------
---
--- Estrutura da tabela `usuarios`
---
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
