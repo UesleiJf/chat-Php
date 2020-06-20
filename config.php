329020013ee23ffe5bfde6fe31ca498056643870
uesleijf <uesleijf@gmail.com>
2020-06-20 01:15:04 -0300

    - COnfig

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 	$nome_do_banco = "chat";
 	$usuario = "root";
 	$senha = "root";
-	//instancia um objeto da classe PDO chamado $conn
+	
 	$conn = new PDO(
 		"$tipo_servidor:host=$servidor;dbname=$nome_do_banco",$usuario,$senha, 
 			array(
