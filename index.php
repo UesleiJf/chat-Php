be04f5bd6bf36b0df733562376adcff55a06dedc
uesleijf <uesleijf@gmail.com>
2020-07-03 16:21:09 -0300

    - Layout Index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
    session_start();
    require_once("config.php"); 
    require_once("functions.php");
@@ -12,7 +13,8 @@
        delete_old_entries();
        start_chat();
    }
-   ?>
+?>
+
 <html>
    <head>
       <title>Chat</title>
